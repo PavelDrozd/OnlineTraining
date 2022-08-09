6e3598237b8237fb617e9cc854015dbbb6fff9dd
PavelDrozd <paveldrozd2575@gmail.com>
2022-08-09 18:25:59 +0300

    Add new commands in CommandRegister

--- src/main/java/App/controller/factory/CommandRegister.java
+++ src/main/java/App/controller/factory/CommandRegister.java
@@ -1,11 +1,39 @@
 package App.controller.factory;
 
 import App.controller.Command;
+import App.controller.commands.course.CourseCommand;
+import App.controller.commands.course.CoursesCommand;
 import App.controller.commands.error.ErrorCommand;
+import App.controller.commands.order.OrderCommand;
+import App.controller.commands.order.OrdersCommand;
+import App.controller.commands.user.session.LoginCommand;
+import App.controller.commands.user.session.LoginFormCommand;
+import App.controller.commands.user.UserCommand;
+import App.controller.commands.user.UsersCommand;
+import App.controller.commands.user.session.LogoutCommand;
+import App.service.CourseService;
+import App.service.OrderService;
+import App.service.factory.ServiceFactory;
+import App.service.UserService;
 
 public enum CommandRegister {
     //ERROR COMMANDS
     ERROR(new ErrorCommand()),
+
+    //USER COMMANDS
+    USER(new UserCommand(ServiceFactory.INSTANCE.getService(UserService.class))),
+    USERS(new UsersCommand(ServiceFactory.INSTANCE.getService(UserService.class))),
+    LOGIN_FORM(new LoginFormCommand()),
+    LOGIN(new LoginCommand(ServiceFactory.INSTANCE.getService(UserService.class))),
+    LOGOUT(new LogoutCommand()),
+
+    //COURSE COMMANDS
+    COURSE(new CourseCommand(ServiceFactory.INSTANCE.getService(CourseService.class))),
+    COURSES(new CoursesCommand(ServiceFactory.INSTANCE.getService(CourseService.class))),
+
+    //ORDERS COMMAND
+    ORDER(new OrderCommand(ServiceFactory.INSTANCE.getService(OrderService.class))),
+    ORDERS(new OrdersCommand(ServiceFactory.INSTANCE.getService(OrderService.class))),
 
 
     ;//
