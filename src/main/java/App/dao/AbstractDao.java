1b5046d9be0c7d063142294d2de3b4e8481c54bf
PavelDrozd <paveldrozd2575@gmail.com>
2022-08-09 14:44:31 +0300

    Create interface abstract DAO with CRUD methods

---
+++ src/main/java/App/dao/AbstractDao.java
@@ -0,0 +1,18 @@
+package App.dao;
+
+import java.util.List;
+
+public interface AbstractDao<K, T> {
+
+    T create (T t);
+
+    List<T> getAll(int limit, long offset);
+
+    T getById(K id);
+
+    T update(T t);
+
+    boolean delete(K id);
+
+    K count ();
+}
