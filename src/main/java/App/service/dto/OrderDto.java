b7243508362341e4f907a4693546ba6db6afd2a4
PavelDrozd <paveldrozd2575@gmail.com>
2022-08-09 15:19:22 +0300

    Create DTO classes Course Order User
    This data classes have the same fields like entities.

---
+++ src/main/java/App/service/dto/OrderDto.java
@@ -0,0 +1,20 @@
+package App.service.dto;
+
+import App.dao.entity.OrderInfo;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class OrderDto {
+    private Long id;
+    private UserDto user;
+    private StatusDto status;
+    private BigDecimal totalCost;
+    private List<OrderInfo> details;
+
+    public enum StatusDto{
+        UNPAID, CANCELLED, PAYED,
+    }
+}
