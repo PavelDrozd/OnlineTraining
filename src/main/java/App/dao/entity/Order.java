b08181aa416d3bdc3613bea2f3d65859e8fd2345
PavelDrozd <paveldrozd2575@gmail.com>
2022-08-09 14:51:30 +0300

    Create entities User, Couser, Order, OrderInfo
    Data classes for which they get their parameters from the database

---
+++ src/main/java/App/dao/entity/Order.java
@@ -0,0 +1,19 @@
+package App.dao.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class Order {
+    private Long id;
+    private User user;
+    private Status status;
+    private BigDecimal totalCost;
+    private List<OrderInfo> details;
+
+    public enum Status{
+        UNPAID, CANCELLED, PAYED,
+    }
+}
