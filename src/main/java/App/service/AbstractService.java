d1f4a9395bcba40f6e46ade69ea5e4a064f33277
PavelDrozd <paveldrozd2575@gmail.com>
2022-08-09 15:14:04 +0300

    Create interface AbstractService
    This interface have CRUD methods like AbstractDao interface.

---
+++ src/main/java/App/service/AbstractService.java
@@ -0,0 +1,18 @@
+package App.service;
+
+import java.util.List;
+
+public interface AbstractService<K,T> {
+
+    T create (T t);
+
+    List<T> getAll(int limit, long offset);
+
+    T getById(K id);
+
+    T update(T t);
+
+    void delete(K id);
+
+    K count();
+}
