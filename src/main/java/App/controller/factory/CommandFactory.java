9ffad3b79fb344de3b8faa30536b7ea0ae068638
PavelDrozd <paveldrozd2575@gmail.com>
2022-08-09 15:53:24 +0300

    Create CommandFactory
    This singletone enum have their own instance and have method getCommand which use CommandRegister enum for call commands and return commandInstance.

---
+++ src/main/java/App/controller/factory/CommandFactory.java
@@ -0,0 +1,19 @@
+package App.controller.factory;
+
+import App.controller.Command;
+
+public enum CommandFactory {
+    INSTANCE;
+
+    CommandFactory(){
+    }
+
+    public Command getCommand(String command){
+        Command commandInstance = CommandRegister.valueOf(command.toUpperCase()).getCommand();
+        if (commandInstance == null){
+            commandInstance = CommandRegister.valueOf("ERROR").getCommand();
+        }
+        return commandInstance;
+    }
+
+}
