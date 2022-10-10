2f0e775f5c34c3f70a60d1b5e1c5392c28ded46d
PavelDrozd <paveldrozd2575@gmail.com>
2022-10-10 21:06:51 +0300

    Add jpa annotation in entity,create role converter

---
+++ src/main/java/App/repository/entity/Order.java
@@ -0,0 +1,76 @@
+package App.repository.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "status_id")
+    private Status status;
+
+    @Column(name = "total_cost")
+    private BigDecimal totalCost;
+
+    @OneToMany(cascade = CascadeType.REFRESH)
+    private List<OrderInfo> details;
+
+    public enum Status {
+        UNPAID, CANCELLED, PAYED,
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Order order = (Order) o;
+        return id != null && Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", user=" + user +
+                ", status=" + status +
+                ", totalCost=" + totalCost +
+                ", details=" + details +
+                '}';
+    }
+}
