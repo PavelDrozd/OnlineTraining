ad01e04a9599943a0b1e94d9ed0be350ccbdb573
PavelDrozd <paveldrozd2575@gmail.com>
2022-10-18 16:39:48 +0300

    Add annotation for work with JpaRepositories

--- src/main/java/app/ContextConfig.java
+++ src/main/java/app/ContextConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,6 +16,7 @@ import javax.persistence.Persistence;
 @ComponentScan
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
+@EnableJpaRepositories
 public class ContextConfig {
 
     @Bean
