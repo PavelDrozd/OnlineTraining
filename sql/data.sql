89b26fb3e1faf2d18f7e11c9d750a54903c02052
PavelDrozd <paveldrozd2575@gmail.com>
2022-11-03 15:11:56 +0300

    Create new sql schema and data

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,8 @@
 /*
-TRUNCATE TABLE order_info CASCADE;
+TRUNCATE TABLE possibilities CASCADE;
 TRUNCATE TABLE orders CASCADE;
+TRUNCATE TABLE lessons CASCADE;
+TRUNCATE TABLE course_progressions CASCADE;
 TRUNCATE TABLE users CASCADE;
 TRUNCATE TABLE roles CASCADE;
 TRUNCATE TABLE courses CASCADE;
@@ -13,28 +15,81 @@ VALUES ('UNPAID'),
         ('CANCELED'),
         ('PAYED');
 
-INSERT INTO courses ("name", "cost", duration_days)
-VALUES ('HISTORY', 25.2, 90),
-		('BIOLOGICAL', 33.2, 45),
-		('CHEMISTRY', 35.1, 60),
-		('MEDICINE', 57.6, 75);
+INSERT INTO courses ("name", course_info, "cost", discount, duration_days)
+VALUES ('HISTORY', 'History info', 25.2, 0, 90),
+		('BIOLOGICAL', 'Biological info', 33.2, 0, 45),
+		('CHEMISTRY', 'Chemistry info', 35.1, 15.1, 60),
+		('MEDICINE', 'Medicine info', 57.6, 27.6, 75);
 
 INSERT INTO roles ("name")
 VALUES ('USER'),
 		('STUDENT'),
-		('TEACHER'),
-		('ADMIN');
+		('TUTOR'),
+		('ADMIN'),
+		('SUPERADMIN');
 
-INSERT INTO users (firstName, lastName, age, email, "password", role_id)
-VALUES ('Adam', 'Admin', 30, 'admin@gmail.com', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM roles WHERE name = 'ADMIN')),
-        ('Teach', 'Teacher', 35, 'teacher@gmail.com', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM roles WHERE name = 'TEACHER')),
-        ('Stud', 'Student', 35, 'student@gmail.com', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM roles WHERE name = 'STUDENT')),
-        ('Us', 'User', 35, 'user@gmail.com', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM roles WHERE name = 'USER'));
+INSERT INTO personal_infos (firstName, lastName, email)
+VALUES('SuperAdam', 'SuperAdmin', 'superadmin@gmail.com'),
+        ('Adam', 'Admin', 'admin@gmail.com'),
+        ('Tutor', 'Tutor', 'tutor@gmail.com'),
+        ('Stud1', 'Student1', 'student1@gmail.com'),
+        ('Stud2', 'Student2', 'student2@gmail.com'),
+        ('Stud3', 'Student3', 'student3@gmail.com'),
+        ('Stud4', 'Student4', 'student4@gmail.com'),
+        ('Us', 'User', 'user@gmail.com');
 
+INSERT INTO users (login, "password", personal_info_id, role_id)
+VALUES('superadmin', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'superadmin@gmail.com'), (SELECT id FROM roles WHERE name = 'SUPERADMIN')),
+        ('admin', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'admin@gmail.com'), (SELECT id FROM roles WHERE name = 'ADMIN')),
+        ('tutor', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'tutor@gmail.com'), (SELECT id FROM roles WHERE name = 'TUTOR')),
+        ('student1', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'student1@gmail.com'), (SELECT id FROM roles WHERE name = 'STUDENT')),
+        ('student2', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'student2@gmail.com'), (SELECT id FROM roles WHERE name = 'STUDENT')),
+        ('student3', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'student3@gmail.com'), (SELECT id FROM roles WHERE name = 'STUDENT')),
+        ('student4', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'student4@gmail.com'), (SELECT id FROM roles WHERE name = 'STUDENT')),
+        ('user', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', (SELECT id FROM personal_infos WHERE email = 'user@gmail.com'), (SELECT id FROM roles WHERE name = 'USER'));
 
-INSERT INTO orders (user_id, status_id, total_cost)
-VALUES ((SELECT id FROM users WHERE email = 'student@gmail.com'), (SELECT id FROM order_status WHERE name = 'UNPAID'), 23.9);
+INSERT INTO possibilities (user_id)
+VALUES ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'superadmin@gmail.com'))),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'admin@gmail.com'))),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'tutor@gmail.com'))),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student1@gmail.com'))),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student2@gmail.com'))),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student3@gmail.com'))),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student4@gmail.com'))),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'user@gmail.com')));
 
-INSERT INTO order_info (course_id, order_id, course_price)
-VALUES ((SELECT id FROM courses WHERE name = 'MEDICINE'), (SELECT id FROM orders WHERE user_id = (SELECT id FROM users WHERE email = 'student@gmail.com')), 22.5);
+INSERT INTO course_progressions (course_id, user_id, completed_lessons)
+VALUES ((SELECT id FROM courses WHERE name = 'HISTORY'), (SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student1@gmail.com')), 0),
+        ((SELECT id FROM courses WHERE name = 'BIOLOGICAL'), (SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student2@gmail.com')), 3),
+        ((SELECT id FROM courses WHERE name = 'CHEMISTRY'), (SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student3@gmail.com')), 4),
+        ((SELECT id FROM courses WHERE name = 'MEDICINE'), (SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student4@gmail.com')), 5);
 
+INSERT INTO lessons ("name", lesson_info, course_progression_id)
+VALUES ('lesson1', 'lesson1_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'BIOLOGICAL'))),
+        ('lesson2', 'lesson2_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'BIOLOGICAL'))),
+        ('lesson3', 'lesson3_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'BIOLOGICAL'))),
+        ('lesson4', 'lesson4_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'BIOLOGICAL'))),
+        ('lesson5', 'lesson5_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'BIOLOGICAL'))),
+        ('lesson1', 'lesson1_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'CHEMISTRY'))),
+        ('lesson2', 'lesson2_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'CHEMISTRY'))),
+        ('lesson3', 'lesson3_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'CHEMISTRY'))),
+        ('lesson4', 'lesson4_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'CHEMISTRY'))),
+        ('lesson5', 'lesson5_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'CHEMISTRY'))),
+        ('lesson6', 'lesson6_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'CHEMISTRY'))),
+        ('lesson7', 'lesson7_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'CHEMISTRY'))),
+        ('lesson1', 'lesson1_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE'))),
+        ('lesson2', 'lesson2_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE'))),
+        ('lesson3', 'lesson3_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE'))),
+        ('lesson4', 'lesson4_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE'))),
+        ('lesson5', 'lesson5_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE'))),
+        ('lesson6', 'lesson6_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE'))),
+        ('lesson7', 'lesson7_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE'))),
+        ('lesson8', 'lesson8_info', (SELECT id FROM course_progressions WHERE course_id = (SELECT id FROM courses WHERE name = 'MEDICINE')));
+
+INSERT INTO orders (user_id, status_id, course_id, total_cost)
+VALUES ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student1@gmail.com')), (SELECT id FROM order_status WHERE name = 'UNPAID'), (SELECT id FROM courses WHERE name = 'HISTORY'), 25.2),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student2@gmail.com')), (SELECT id FROM order_status WHERE name = 'PAYED'), (SELECT id FROM courses WHERE name = 'BIOLOGICAL'), 33.2),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student3@gmail.com')), (SELECT id FROM order_status WHERE name = 'PAYED'), (SELECT id FROM courses WHERE name = 'CHEMISTRY'), 20.0),
+        ((SELECT id FROM users WHERE personal_info_id = (SELECT id FROM personal_infos WHERE email = 'student4@gmail.com')), (SELECT id FROM order_status WHERE name = 'PAYED'), (SELECT id FROM courses WHERE name = 'MEDICINE'), 30.0);
+
+UPDATE possibilities SET watch_users = TRUE,delete_users = TRUE,change_role_users = TRUE,change_users_parameters = TRUE,create_courses = TRUE,delete_courses = TRUE,change_courses = TRUE,watch_deleted_courses = TRUE,watch_users_orders = TRUE,change_orders_status = TRUE,create_lessons = TRUE,watch_lessons = TRUE,watch_deleted_lessons = TRUE,delete_lessons = TRUE,change_lessons = TRUE WHERE user_id = (SELECT id FROM roles WHERE name = 'SUPERADMIN');
