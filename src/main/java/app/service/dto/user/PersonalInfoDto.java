57f026809055ad12b0a674ad3df30fc098d553d4
PavelDrozd <paveldrozd2575@gmail.com>
2022-11-06 18:52:45 +0300

    Add validation in service dto classes

--- src/main/java/app/service/dto/user/PersonalInfoDto.java
+++ src/main/java/app/service/dto/user/PersonalInfoDto.java
@@ -3,6 +3,8 @@ package app.service.dto.user;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -10,12 +12,20 @@ public class PersonalInfoDto {
 
     private Long id;
 
+    @NotBlank(message = "Firstname is required")
+    @Size(max = 15, message = "Firstname is too long")
     private String firstName;
 
+    @NotBlank(message = "Lastname is required")
+    @Size(max = 15, message = "Lastname is too long")
     private String lastName;
 
+    @Size(max = 15, message = "Patronymic is too long")
     private String patronymic;
 
+    @NotBlank(message = "Email is required")
+    @Size(max = 35, message = "Email is too long")
+    @Size(min = 5, message = "Email is too short")
     private String email;
 
     private LocalDate dayOfBirth;
--- src/main/java/app/service/dto/user/UserDto.java
+++ src/main/java/app/service/dto/user/UserDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
@@ -11,10 +12,14 @@ public class UserDto {
 
     private Long id;
 
-    @Size(min = 5, max = 35)
+    @NotBlank(message = "Login is required")
+    @Size(min = 5, message = "TLogin is too short")
+    @Size(max = 35, message = "Login is too long")
     private String login;
 
-    @Size(min = 4, max = 35)
+    @NotBlank(message = "Password is required")
+    @Size(min = 4, message = "Password is too short")
+    @Size(max = 35, message = "Password is too long")
     @JsonIgnore
     private String password;
 
