f49af1a7d53a68e3988daaf4d3c58e0721e0c3e8
PavelDrozd <paveldrozd2575@gmail.com>
2022-11-10 16:39:39 +0300

    Create DTOs for error and result validation

---
+++ src/main/java/app/service/dto/errors/ValidationResultDto.java
@@ -0,0 +1,20 @@
+package app.service.dto.errors;
+
+import java.util.List;
+import java.util.Map;
+
+public class ValidationResultDto extends ErrorDto {
+
+    private static final String DEFAULT_ERROR = "Validation error";
+    private static final String DEFAULT_ERROR_MESSAGE = "Sent data violates validation constraints";
+    private Map<String, List<String>> messages;
+
+    public ValidationResultDto() {
+        super(DEFAULT_ERROR, DEFAULT_ERROR_MESSAGE);
+    }
+
+    public ValidationResultDto(Map<String, List<String>> messages) {
+        this();
+        this.messages = messages;
+    }
+}
