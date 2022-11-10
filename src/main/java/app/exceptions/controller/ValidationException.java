8f9905d9715e168892c6643d43139afd8989d3b9
PavelDrozd <paveldrozd2575@gmail.com>
2022-11-10 16:38:03 +0300

    Create new exception structure

---
+++ src/main/java/app/exceptions/controller/ValidationException.java
@@ -0,0 +1,14 @@
+package app.exceptions.controller;
+
+import lombok.Getter;
+import org.springframework.validation.Errors;
+
+public class ValidationException extends ControllerException {
+    @Getter
+    private final Errors errors;
+
+    public ValidationException(Errors errors) {
+        this.errors = errors;
+    }
+
+}
